daa5c83c0e2a0a76402ed655d0f845fcd0d98504
Kenny Capote <kenny.capote@gmail.com>
2018-08-29 12:21:03 -0300

    cambios

--- src/app/util/util.ts
+++ src/app/util/util.ts
@@ -2,7 +2,7 @@ import { FormControl } from "@angular/forms";
 
 export class Util {
     //public static URL_SERVER = 'http://180.124.152.20:3000';
-    public static URL_SERVER = 'http://localhost:3000';
+    public static URL_SERVER = 'http://180.124.83.49:3000';
 
     public static  URL_CATEGORIAS  = `${ Util.URL_SERVER }/category`;
     public static  URL_MANUAL  = `${ Util.URL_SERVER }/manual`;
